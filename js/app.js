c588746ece6c4a995f5edd32dbe97afc5e3662ce
Gian <giancastrovilca@gmail.com>
2023-02-06 11:49:33 -0500

    carrito

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
 const listaCursos = document.querySelector('#lista-cursos');
 
+let articulosCarrito = [];
+
 
 
 cargarEventListeners()
@@ -16,5 +18,53 @@ function cargarEventListeners() {
 // funciones
 
 function agregarCurso(e) {
-    console.log(e.target.classList);
+    e.preventDefault();
+    // para detener el scroll automativo
+    if(e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+
+        leerDatosCurso(cursoSeleccionado);
+    };
 }
+
+// lee el contenido del HTML
+function leerDatosCurso(curso) {
+    // console.log(curso);
+
+    // crear un objeto
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('button').getAttribute('data-id'),
+        cantidad: 1,
+    }
+
+    // console.log(infoCurso);
+    // agregar elementos al arreglo carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+
+    console.log(articulosCarrito);
+    carritoHTML()
+}
+
+// muestra el carrito de compras en el HTML
+function carritoHTML() {
+
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+
+        // agregar el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+
+// function agregarCurso(e){
+//     console.log(e.target.classList);
+// }
